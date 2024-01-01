f7b17922be213e7ca3fea25daca19a0cc32f720e
Kasun M <kasunldrago@gmail.com>
2024-01-02 01:04:38 +0530

    Infromation and Structure added

---
+++ app/components/about.tsx
@@ -0,0 +1,35 @@
+export default function About() {
+  return (
+    <div className="p-2 md:p-8">
+      <h1 className="text-xl">Hello, I&apos;m</h1>
+      <h2 className="text-5xl">Kasun Mendis,</h2>
+      <p>
+        an avid and curious learner delving into computer science, constantly refining my skills to embrace the ever-evolving technological landscape. I'm interested in machine learning and game development, and I find joy in unraveling how these domains work. Beyond code, I am partial to minimalistic designs, appreciating the artistry that simplicity can bring to both technology and aesthetics. I'm a Final Year Computer Science Undergraduate finishing up my bachelor's at <a href="https://ucsc.cmb.ac.lk/">UCSC</a>.
+      </p>
+      <p className="pt-4">
+        <g className="text-xl">Technology</g> I'm familiar with,
+      </p>
+      <div className="overflow-x-auto">
+        <table className="table w-80">
+          <tbody>
+            {/* row 1 */}
+            <tr className="border-none">
+              <td>&#9900; Javascript</td>
+              <td>&#9900; Typescript</td>
+            </tr>
+            {/* row 2 */}
+            <tr className="border-none">
+              <td>&#9900; NodeJS</td>
+              <td>&#9900; NestJS</td>
+            </tr>
+            {/* row 3 */}
+            <tr className="border-none">
+              <td>&#9900; React</td>
+              <td>&#9900; Python</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+}
