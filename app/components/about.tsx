acd2181728a7f1535e312df173b3be17100307af
Kasun M <kasunldrago@gmail.com>
2024-04-25 00:53:34 +0530

    updated: projects and about

--- app/components/about.tsx
+++ app/components/about.tsx
@@ -4,28 +4,25 @@ export default function About() {
       <h1 className="text-xl">Hello, I&apos;m</h1>
       <h2 className="text-5xl">Kasun Mendis,</h2>
       <p>
-        an avid and curious learner delving into computer science, constantly refining my skills to embrace the ever-evolving technological landscape. I&apos;m interested in machine learning and game development, and I find joy in unraveling how these domains work. Beyond code, I am partial to minimalistic designs, appreciating the artistry that simplicity can bring to both technology and aesthetics. I&apos;m a Final Year Computer Science Undergraduate finishing up my bachelor&apos;s at <a href="https://ucsc.cmb.ac.lk/">UCSC</a>.
+        an avid and curious learner delving into computer science, constantly refining my skills to embrace the ever-evolving technological landscape. I&apos;m interested in machine learning and game development, and I find joy in unraveling how these domains work. Beyond code, I am partial to minimalistic designs, appreciating the artistry that simplicity can bring to both technology and aesthetics. I&apos;m Computer Science graduate at <a href="https://ucsc.cmb.ac.lk/">UCSC</a>.
       </p>
       <p className="pt-4">
         <g className="text-xl">Technology</g> I&apos;m familiar with,
       </p>
       <div className="overflow-x-auto">
-        <table className="table w-80">
+        <table className="table w-100">
           <tbody>
             {/* row 1 */}
             <tr className="border-none">
-              <td>&#9900; Javascript</td>
-              <td>&#9900; Typescript</td>
+              <td>
+                Programming <br /> Python, PHP, JavaScript, TypeScript, C/C++, HTML/CSS, SQL
+              </td>
             </tr>
             {/* row 2 */}
             <tr className="border-none">
-              <td>&#9900; NodeJS</td>
-              <td>&#9900; NestJS</td>
-            </tr>
-            {/* row 3 */}
-            <tr className="border-none">
-              <td>&#9900; React</td>
-              <td>&#9900; Python</td>
+              <td>
+                Technologies <br /> NodeJS, NestJS, ReactJS, TensorFlow, Scikit-learn, Pandas, TailwindCSS, MongoDB, Git, Postman, Figma
+              </td>
             </tr>
           </tbody>
         </table>
